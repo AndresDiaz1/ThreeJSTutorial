2b5d47225713a66f2a150c97d90639ebdbfa0c05
Andrés  Díaz Vargas <andres.diaz.vargas@gorillalogic.com>
2021-09-15 11:57:26 -0500

    added custom buffer geometry element

--- 2. Webpack/src/script.js
+++ 2. Webpack/src/script.js
@@ -4,6 +4,29 @@ import gsap from 'gsap'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 
+window.addEventListener('resize',(e)=>{
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerWidth;
+
+    camera.aspect = sizes.width/sizes.height
+
+    //Update Camera
+    camera.updateProjectionMatrix();
+    
+    //Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+});
+
+window.addEventListener('dblclick', ()=>{
+     if (!document.fullscreenElement) {
+        canvas.requestFullscreen()
+     }else  {
+        document.exitFullscreen()
+     }
+})
+
 const scene = new THREE.Scene();
 
 // Red Cube
@@ -33,10 +56,15 @@ const cube2 = new THREE.Mesh(
 );
 cube2.position.x = 2;
 const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({color: 0x0000ff})
+    new THREE.BoxGeometry(1, 1, 1, 2, 2, 2),
+    new THREE.MeshBasicMaterial({
+        color: 0x0000ff,
+        wireframe: true
+    })
 );
 cube3.position.x = -2;
+
+cube1.position.y = -3
 
 group.add(cube1);
 group.add(cube2);
@@ -46,13 +74,66 @@ group.rotation.y = Math.PI / 6;
 
 // Camera
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,                                                                                                      
+    height: window.innerHeight
 }
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
 scene.add(camera);
+
+
+// Custom geometry
+
+// const positionsArray = new Float32Array(9)
+
+// // First vertice
+// positionsArray[0] = 0
+// positionsArray[1] = 0
+// positionsArray[2] = 0
+
+// // Second vertice
+// positionsArray[3] = 0
+// positionsArray[4] = 1
+// positionsArray[5] = 0
+
+// // Third vertice
+// positionsArray[6] = 1
+// positionsArray[7] = 0
+// positionsArray[8] = 0
+
+//          ∆
+//          |
+//       same as:
+const positionsArray = new Float32Array([
+    0, 0 ,0,
+    0, 1, 0,
+    1, 0, 0
+]);
+
+const positionAttribute = new THREE.BufferAttribute(positionsArray, 3)
+const customGeometry = new THREE.BufferGeometry();
+customGeometry.setAttribute('position', positionAttribute)
+const customGeometryMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
+
+const meshCustomGeometry = new THREE.Mesh(customGeometry, customGeometryMaterial);
+scene.add(meshCustomGeometry);
+
+
+const count = 50
+const customGeometryRandom = new THREE.BufferGeometry();
+const positionsArrayRandom = new Float32Array(count * 3 * 3)
+
+for (let i=0; i< count *3 *3; i++) {
+    positionsArrayRandom[i] = Math.random() -0.5
+}
+
+const positionsAttributeRandom = new THREE.BufferAttribute(positionsArrayRandom, 3)
+customGeometryRandom.setAttribute('position', positionsAttributeRandom)
+
+const meshCustomRandomGeometry = new THREE.Mesh(customGeometryRandom, customGeometryMaterial);
+meshCustomRandomGeometry.position.x =2
+scene.add(meshCustomRandomGeometry);
 
 // Renderer
 const canvas = document.querySelector('canvas.webgl');
@@ -60,7 +141,7 @@ const renderer = new THREE.WebGLRenderer({
     canvas
 });
 renderer.setSize(sizes.width, sizes.height);
-
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 
 // Green Socket animation
